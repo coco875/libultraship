3d963edc2afeb86cd81d56e1613163f12ae57301
coco875 <59367621+coco875@users.noreply.github.com>
2025-04-08 14:33:09 +0200

    start implement shader

--- src/graphic/Fast3D/gfx_llgl.cpp
+++ src/graphic/Fast3D/gfx_llgl.cpp
@@ -10,6 +10,8 @@
 #endif
 
 #include <LLGL/Backend/OpenGL/NativeHandle.h>
+#include "../../resource/type/Shader.h"
+#include <Context.h>
 
 LLGL::RenderSystemPtr llgl_renderer;
 LLGL::SwapChain* llgl_swapChain;
@@ -19,6 +21,8 @@ struct {
     int current_tile;
     uint32_t current_texture_ids[SHADER_MAX_TEXTURES];
     std::vector<LLGL::Texture*> textures;
+    bool srgb_mode = false;
+    FilteringMode current_filter_mode = FILTER_NONE;
 } llgl_state;
 
 const char* gfx_llgl_get_name(void) {
@@ -41,6 +45,305 @@ void gfx_llgl_unload_shader(struct ShaderProgram* old_prg) {
 void gfx_llgl_load_shader(struct ShaderProgram* new_prg) {
 }
 
+static void llgl_append_str(char* buf, size_t* len, const char* str) {
+    while (*str != '\0') {
+        buf[(*len)++] = *str++;
+    }
+}
+
+static void llgl_append_line(char* buf, size_t* len, const char* str) {
+    while (*str != '\0') {
+        buf[(*len)++] = *str++;
+    }
+    buf[(*len)++] = '\n';
+}
+
+#define RAND_NOISE "((random(vec3(floor(gl_FragCoord.xy * noise_scale), float(frame_count))) + 1.0) / 2.0)"
+
+static const char* llgl_shader_item_to_str(uint32_t item, bool with_alpha, bool only_alpha, bool inputs_have_alpha,
+                                           bool first_cycle, bool hint_single_element) {
+    if (!only_alpha) {
+        switch (item) {
+            case SHADER_0:
+                return with_alpha ? "vec4(0.0, 0.0, 0.0, 0.0)" : "vec3(0.0, 0.0, 0.0)";
+            case SHADER_1:
+                return with_alpha ? "vec4(1.0, 1.0, 1.0, 1.0)" : "vec3(1.0, 1.0, 1.0)";
+            case SHADER_INPUT_1:
+                return with_alpha || !inputs_have_alpha ? "vInput1" : "vInput1.rgb";
+            case SHADER_INPUT_2:
+                return with_alpha || !inputs_have_alpha ? "vInput2" : "vInput2.rgb";
+            case SHADER_INPUT_3:
+                return with_alpha || !inputs_have_alpha ? "vInput3" : "vInput3.rgb";
+            case SHADER_INPUT_4:
+                return with_alpha || !inputs_have_alpha ? "vInput4" : "vInput4.rgb";
+            case SHADER_TEXEL0:
+                return first_cycle ? (with_alpha ? "texVal0" : "texVal0.rgb")
+                                   : (with_alpha ? "texVal1" : "texVal1.rgb");
+            case SHADER_TEXEL0A:
+                return first_cycle
+                           ? (hint_single_element ? "texVal0.a"
+                                                  : (with_alpha ? "vec4(texVal0.a, texVal0.a, texVal0.a, texVal0.a)"
+                                                                : "vec3(texVal0.a, texVal0.a, texVal0.a)"))
+                           : (hint_single_element ? "texVal1.a"
+                                                  : (with_alpha ? "vec4(texVal1.a, texVal1.a, texVal1.a, texVal1.a)"
+                                                                : "vec3(texVal1.a, texVal1.a, texVal1.a)"));
+            case SHADER_TEXEL1A:
+                return first_cycle
+                           ? (hint_single_element ? "texVal1.a"
+                                                  : (with_alpha ? "vec4(texVal1.a, texVal1.a, texVal1.a, texVal1.a)"
+                                                                : "vec3(texVal1.a, texVal1.a, texVal1.a)"))
+                           : (hint_single_element ? "texVal0.a"
+                                                  : (with_alpha ? "vec4(texVal0.a, texVal0.a, texVal0.a, texVal0.a)"
+                                                                : "vec3(texVal0.a, texVal0.a, texVal0.a)"));
+            case SHADER_TEXEL1:
+                return first_cycle ? (with_alpha ? "texVal1" : "texVal1.rgb")
+                                   : (with_alpha ? "texVal0" : "texVal0.rgb");
+            case SHADER_COMBINED:
+                return with_alpha ? "texel" : "texel.rgb";
+            case SHADER_NOISE:
+                return with_alpha ? "vec4(" RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ")"
+                                  : "vec3(" RAND_NOISE ", " RAND_NOISE ", " RAND_NOISE ")";
+        }
+    } else {
+        switch (item) {
+            case SHADER_0:
+                return "0.0";
+            case SHADER_1:
+                return "1.0";
+            case SHADER_INPUT_1:
+                return "vInput1.a";
+            case SHADER_INPUT_2:
+                return "vInput2.a";
+            case SHADER_INPUT_3:
+                return "vInput3.a";
+            case SHADER_INPUT_4:
+                return "vInput4.a";
+            case SHADER_TEXEL0:
+                return first_cycle ? "texVal0.a" : "texVal1.a";
+            case SHADER_TEXEL0A:
+                return first_cycle ? "texVal0.a" : "texVal1.a";
+            case SHADER_TEXEL1A:
+                return first_cycle ? "texVal1.a" : "texVal0.a";
+            case SHADER_TEXEL1:
+                return first_cycle ? "texVal1.a" : "texVal0.a";
+            case SHADER_COMBINED:
+                return "texel.a";
+            case SHADER_NOISE:
+                return RAND_NOISE;
+        }
+    }
+    return "";
+}
+
+bool llgl_get_bool(prism::ContextTypes* value) {
+    if (std::holds_alternative<bool>(*value)) {
+        return std::get<bool>(*value);
+    } else if (std::holds_alternative<int>(*value)) {
+        return std::get<int>(*value) == 1;
+    }
+    return false;
+}
+
+prism::ContextTypes* llgl_append_formula(prism::ContextTypes* a_arg, prism::ContextTypes* a_single,
+                                         prism::ContextTypes* a_mult, prism::ContextTypes* a_mix,
+                                         prism::ContextTypes* a_with_alpha, prism::ContextTypes* a_only_alpha,
+                                         prism::ContextTypes* a_alpha, prism::ContextTypes* a_first_cycle) {
+    auto c = std::get<prism::MTDArray<int>>(*a_arg);
+    bool do_single = llgl_get_bool(a_single);
+    bool do_multiply = llgl_get_bool(a_mult);
+    bool do_mix = llgl_get_bool(a_mix);
+    bool with_alpha = llgl_get_bool(a_with_alpha);
+    bool only_alpha = llgl_get_bool(a_only_alpha);
+    bool opt_alpha = llgl_get_bool(a_alpha);
+    bool first_cycle = llgl_get_bool(a_first_cycle);
+    std::string out = "";
+    if (do_single) {
+        out += llgl_shader_item_to_str(c.at(only_alpha, 3), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+    } else if (do_multiply) {
+        out += llgl_shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += " * ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+    } else if (do_mix) {
+        out += "mix(";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 1), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ", ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ", ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+        out += ")";
+    } else {
+        out += "(";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 0), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += " - ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 1), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+        out += ") * ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 2), with_alpha, only_alpha, opt_alpha, first_cycle, true);
+        out += " + ";
+        out += llgl_shader_item_to_str(c.at(only_alpha, 3), with_alpha, only_alpha, opt_alpha, first_cycle, false);
+    }
+    return new prism::ContextTypes{ out };
+}
+
+std::optional<std::string> llgl_opengl_include_fs(const std::string& path) {
+    auto init = std::make_shared<Ship::ResourceInitData>();
+    init->Type = (uint32_t)Ship::ResourceType::Shader;
+    init->ByteOrder = Ship::Endianness::Native;
+    init->Format = RESOURCE_FORMAT_BINARY;
+    auto res = static_pointer_cast<Ship::Shader>(
+        Ship::Context::GetInstance()->GetResourceManager()->LoadResource(path, true, init));
+    if (res == nullptr) {
+        return std::nullopt;
+    }
+    auto inc = static_cast<std::string*>(res->GetRawPointer());
+    return *inc;
+}
+
+static std::string llgl_build_fs_shader(const CCFeatures& cc_features) {
+    prism::Processor processor;
+    prism::ContextItems context = {
+        { "o_c", M_ARRAY(cc_features.c, int, 2, 2, 4) },
+        { "o_alpha", cc_features.opt_alpha },
+        { "o_fog", cc_features.opt_fog },
+        { "o_texture_edge", cc_features.opt_texture_edge },
+        { "o_noise", cc_features.opt_noise },
+        { "o_2cyc", cc_features.opt_2cyc },
+        { "o_alpha_threshold", cc_features.opt_alpha_threshold },
+        { "o_invisible", cc_features.opt_invisible },
+        { "o_grayscale", cc_features.opt_grayscale },
+        { "o_textures", M_ARRAY(cc_features.used_textures, bool, 2) },
+        { "o_masks", M_ARRAY(cc_features.used_masks, bool, 2) },
+        { "o_blend", M_ARRAY(cc_features.used_blend, bool, 2) },
+        { "o_clamp", M_ARRAY(cc_features.clamp, bool, 2, 2) },
+        { "o_inputs", cc_features.num_inputs },
+        { "o_do_mix", M_ARRAY(cc_features.do_mix, bool, 2, 2) },
+        { "o_do_single", M_ARRAY(cc_features.do_single, bool, 2, 2) },
+        { "o_do_multiply", M_ARRAY(cc_features.do_multiply, bool, 2, 2) },
+        { "o_color_alpha_same", M_ARRAY(cc_features.color_alpha_same, bool, 2) },
+        { "o_current_filter", llgl_state.current_filter_mode },
+        { "FILTER_THREE_POINT", FILTER_THREE_POINT },
+        { "FILTER_LINEAR", FILTER_LINEAR },
+        { "FILTER_NONE", FILTER_NONE },
+        { "srgb_mode", llgl_state.srgb_mode },
+        { "SHADER_0", SHADER_0 },
+        { "SHADER_INPUT_1", SHADER_INPUT_1 },
+        { "SHADER_INPUT_2", SHADER_INPUT_2 },
+        { "SHADER_INPUT_3", SHADER_INPUT_3 },
+        { "SHADER_INPUT_4", SHADER_INPUT_4 },
+        { "SHADER_INPUT_5", SHADER_INPUT_5 },
+        { "SHADER_INPUT_6", SHADER_INPUT_6 },
+        { "SHADER_INPUT_7", SHADER_INPUT_7 },
+        { "SHADER_TEXEL0", SHADER_TEXEL0 },
+        { "SHADER_TEXEL0A", SHADER_TEXEL0A },
+        { "SHADER_TEXEL1", SHADER_TEXEL1 },
+        { "SHADER_TEXEL1A", SHADER_TEXEL1A },
+        { "SHADER_1", SHADER_1 },
+        { "SHADER_COMBINED", SHADER_COMBINED },
+        { "SHADER_NOISE", SHADER_NOISE },
+        { "append_formula", (InvokeFunc)llgl_append_formula },
+#ifdef __APPLE__
+        { "GLSL_VERSION", "#version 410 core" },
+        { "attr", "in" },
+        { "opengles", false },
+        { "core_opengl", true },
+        { "texture", "texture" },
+        { "vOutColor", "vOutColor" },
+#elif defined(USE_OPENGLES)
+        { "GLSL_VERSION", "#version 300 es\nprecision mediump float;" },
+        { "attr", "in" },
+        { "opengles", true },
+        { "core_opengl", false },
+        { "texture", "texture" },
+        { "vOutColor", "vOutColor" },
+#else
+        { "GLSL_VERSION", "#version 130" },
+        { "attr", "varying" },
+        { "opengles", false },
+        { "core_opengl", false },
+        { "texture", "texture2D" },
+        { "vOutColor", "gl_FragColor" },
+#endif
+    };
+    processor.populate(context);
+    auto init = std::make_shared<Ship::ResourceInitData>();
+    init->Type = (uint32_t)Ship::ResourceType::Shader;
+    init->ByteOrder = Ship::Endianness::Native;
+    init->Format = RESOURCE_FORMAT_BINARY;
+    auto res = static_pointer_cast<Ship::Shader>(Ship::Context::GetInstance()->GetResourceManager()->LoadResource(
+        "shaders/opengl/default.shader.fs", true, init));
+
+    if (res == nullptr) {
+        SPDLOG_ERROR("Failed to load default fragment shader, missing f3d.o2r?");
+        abort();
+    }
+
+    auto shader = static_cast<std::string*>(res->GetRawPointer());
+    processor.load(*shader);
+    processor.bind_include_loader(llgl_opengl_include_fs);
+    auto result = processor.process();
+    // SPDLOG_INFO("=========== FRAGMENT SHADER ============");
+    // SPDLOG_INFO(result);
+    // SPDLOG_INFO("========================================");
+    return result;
+}
+
+static size_t num_floats = 0;
+
+prism::ContextTypes* llgl_update_floats(prism::ContextTypes* num) {
+    num_floats += std::get<int>(*num);
+    return nullptr;
+}
+
+static std::string llgl_build_vs_shader(const CCFeatures& cc_features) {
+    num_floats = 4;
+    prism::Processor processor;
+    prism::ContextItems context = { { "o_textures", M_ARRAY(cc_features.used_textures, bool, 2) },
+                                    { "o_clamp", M_ARRAY(cc_features.clamp, bool, 2, 2) },
+                                    { "o_fog", cc_features.opt_fog },
+                                    { "o_grayscale", cc_features.opt_grayscale },
+                                    { "o_alpha", cc_features.opt_alpha },
+                                    { "o_inputs", cc_features.num_inputs },
+                                    { "update_floats", (InvokeFunc)llgl_update_floats },
+#ifdef __APPLE__
+                                    { "GLSL_VERSION", "#version 410 core" },
+                                    { "attr", "in" },
+                                    { "out", "out" },
+                                    { "opengles", false }
+#elif defined(USE_OPENGLES)
+                                    { "GLSL_VERSION", "#version 300 es" },
+                                    { "attr", "in" },
+                                    { "out", "out" },
+                                    { "opengles", true }
+#else
+                                    { "GLSL_VERSION", "#version 110" },
+                                    { "attr", "attribute" },
+                                    { "out", "varying" },
+                                    { "opengles", false }
+#endif
+    };
+    processor.populate(context);
+
+    auto init = std::make_shared<Ship::ResourceInitData>();
+    init->Type = (uint32_t)Ship::ResourceType::Shader;
+    init->ByteOrder = Ship::Endianness::Native;
+    init->Format = RESOURCE_FORMAT_BINARY;
+    auto res = static_pointer_cast<Ship::Shader>(Ship::Context::GetInstance()->GetResourceManager()->LoadResource(
+        "shaders/opengl/default.shader.vs", true, init));
+
+    if (res == nullptr) {
+        SPDLOG_ERROR("Failed to load default vertex shader, missing f3d.o2r?");
+        abort();
+    }
+
+    auto shader = static_cast<std::string*>(res->GetRawPointer());
+    processor.load(*shader);
+    processor.bind_include_loader(llgl_opengl_include_fs);
+    auto result = processor.process();
+    // SPDLOG_INFO("=========== VERTEX SHADER ============");
+    // SPDLOG_INFO(result);
+    // SPDLOG_INFO("========================================");
+    return result;
+}
+
 struct ShaderProgram* gfx_llgl_create_and_load_new_shader(uint64_t shader_id0, uint32_t shader_id1) {
     return nullptr;
 }
@@ -58,11 +361,13 @@ uint32_t gfx_llgl_new_texture(void) {
 }
 
 void gfx_llgl_select_texture(int tile, uint32_t texture_id) {
+    // TODO: not finish
     llgl_state.current_tile = tile;
     llgl_state.current_texture_ids[tile] = texture_id;
 }
 
 void gfx_llgl_upload_texture(const uint8_t* rgba32_buf, uint32_t width, uint32_t height) {
+    // TODO: not finish
     LLGL::ImageView imageView;
     imageView.format = LLGL::ImageFormat::RGBA;
     imageView.data = rgba32_buf;
@@ -159,10 +464,8 @@ void gfx_llgl_copy_framebuffer(int fb_dst_id, int fb_src_id, int srcX0, int srcY
 }
 
 void gfx_llgl_clear_framebuffer(bool color, bool depth) {
-    llgl_cmdBuffer->Clear(0 | color   ? LLGL::ClearFlags::Color
-                          : 0 | depth ? LLGL::ClearFlags::Depth
-                                      : 0,
-                          LLGL::ClearValue{ 0.0f, 0.2f, 0.2f, 1.0f });
+    llgl_cmdBuffer->Clear(0 | (color ? LLGL::ClearFlags::Color : 0) | (depth ? LLGL::ClearFlags::Depth : 0),
+                          LLGL::ClearValue{ 0.0f, 0.0f, 0.0f, 1.0f });
 }
 
 void gfx_llgl_read_framebuffer_to_cpu(int fb_id, uint32_t width, uint32_t height, uint16_t* rgba16_buf) {
@@ -187,13 +490,15 @@ void gfx_llgl_delete_texture(uint32_t texture_id) {
 }
 
 void gfx_llgl_set_texture_filter(FilteringMode mode) {
+    llgl_state.current_filter_mode = mode;
 }
 
 FilteringMode gfx_llgl_get_texture_filter(void) {
-    return FILTER_NONE;
+    return llgl_state.current_filter_mode;
 }
 
 void gfx_llgl_enable_srgb_mode(void) {
+    llgl_state.srgb_mode = true;
 }
 
 struct GfxRenderingAPI gfx_llgl_api = { gfx_llgl_get_name,
