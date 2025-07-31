5e41ab67138110206c0d7dcf113e0ad11d79265e
KiritoDv <kiritodev01@gmail.com>
2025-07-31 01:53:06 -0600

    Reworked keyboard system on switch

--- src/window/gui/Gui.cpp
+++ src/window/gui/Gui.cpp
@@ -132,6 +132,7 @@ void Gui::Init(GuiWindowInitData windowImpl) {
     mImGuiIo->Fonts->AddFontFromMemoryCompressedBase85TTF(fontawesome_compressed_data_base85, iconFontSize,
                                                           &iconsConfig, sIconsRanges);
 #ifdef __SWITCH__
+    Ship::Switch::CreateKeyboard();
     Ship::Switch::ImGuiSetupFont(mImGuiIo->Fonts);
 #endif
 
